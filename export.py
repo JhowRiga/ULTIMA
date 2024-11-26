0bab9af3159961206aa39925e508b17d2dd36f0d
JhowRiga <jhowfile@gmail.com>
2024-11-26 13:02:46 -0300

    export.py

--- export.py
+++ export.py
@@ -2,9 +2,11 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 
 def parse_xml_to_dataframe(xml_file):
+    # Parse o arquivo XML
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
+    # Crie listas para armazenar os dados
     data = {
         'codigo': [],
         'nome': [],
@@ -16,6 +18,7 @@ def parse_xml_to_dataframe(xml_file):
         'Valor Total Máximo Aceitável': []
     }
 
+    # Percorra os elementos do XML e extraia os dados relevantes
     for item in root.findall('.//item'):
         codigo = item.find('codigo').text if item.find('codigo') is not None else ''
         nome = item.find('nome').text if item.find('nome') is not None else ''
@@ -28,13 +31,16 @@ def parse_xml_to_dataframe(xml_file):
         data['comentario'].append(comentario)
         data['preco'].append(preco)
         data['quantidade'].append(quantidade)
-        data['desconto (%)'].append(None)
-        data['Valor Unitário Máximo Aceitável'].append(None)
-        data['Valor Total Máximo Aceitável'].append(None)
+        data['desconto (%)'].append(None)  # Coluna vazia
+        data['Valor Unitário Máximo Aceitável'].append(None)  # Coluna vazia
+        data['Valor Total Máximo Aceitável'].append(None)  # Coluna vazia
 
+    # Crie um DataFrame a partir dos dados
     df = pd.DataFrame(data)
+
     return df
 
+# Exemplo de uso
 xml_file = 'seu_arquivo.xml'
 df = parse_xml_to_dataframe(xml_file)
 df.to_json('saida.json', orient='records', lines=True)
